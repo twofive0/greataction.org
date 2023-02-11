647294de60e37bde5983a669d39765bace5e73cb
Devin Williams <devin@greataction.org>
2023-02-11 09:09:11 -0600

    Basic python app added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # greataction.org
 Blog website for greataction.org
+
+This is a (slow) work in progress...
---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask import send_from_directory, render_template
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "jimmyhat250"  # Change this!
+jwt = JWTManager(app)
+
+
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "dwilliams" or password != "developmentpassword":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
+
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+@app.route("/", methods=["GET"])
+def index():
+    return send_from_directory('assets', 'html/index.html')
+
+
+if __name__ == "__main__":
+    app.run()
